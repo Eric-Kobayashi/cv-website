839eadfb9d40ae100ce97d955d9d55d9cb0286f5
Eric Kobayashi <33022148+Eric-Kobayashi@users.noreply.github.com>
2025-08-09 02:52:10 +0100

    Metrics: use DOI-driven Altmetric/Dimensions with tinier inline badges and proper link targets

--- assets/js/site.js
+++ assets/js/site.js
@@ -211,10 +211,10 @@ async function populateSite() {
           } catch {}
         }
         const altmetricSpan = doi
-          ? `<span class="altmetric-embed" data-badge-type="donut" data-doi="${doi}" data-hide-no-mentions="true"></span>`
+          ? `<span class="altmetric-embed" data-badge-type="donut" data-doi="${doi}" data-link-target="_blank" data-hide-no-mentions="true"></span>`
           : '';
         const dimensionsSpan = doi
-          ? `<span class="__dimensions_badge_embed__" data-doi="${doi}" data-style="small_circle"></span>`
+          ? `<span class="__dimensions_badge_embed__" data-doi="${doi}" data-style="small_circle" data-legend="never" data-hide-zero-citations="true"></span>`
           : '';
 
         const buttonsHtml = `
